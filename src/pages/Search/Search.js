427c1130cde6386d321eb6601dee0766bc6f83bb
DeeFG <xdeishax@gmail.com>
2019-08-13 00:31:43 -0400

    dispalying..load issue

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -49,11 +49,13 @@ class Search extends Component {
     return (
       <div>
         <Jumbotron>
-          <h1 className="display-4">(React) Google Books Search</h1>
+          <h1 className="display-4">Google Books Search</h1>
           <p className="lead">Search for and save books of interest.</p>
           <hr className="my-4" />
           <p className="lead">
             <Link className="btn btn-default btn-lg" to="/" role="button">New Search</Link>
+            </p>
+            <p className="lead">
             <Link className="btn btn-default btn-lg" to="/saved" role="button">Saved Books</Link>
           </p>
         </Jumbotron>
